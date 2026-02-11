5b38849e6e89d6666b2b6b0739707b0cd6c57203
Matti Kähkönen <matti.kahkonen99@gmail.com>
2026-02-11 16:02:05 +0200

    add MaxDays constraint encoding

--- src/semantics/scheduling-semantics.cpp
+++ src/semantics/scheduling-semantics.cpp
@@ -1378,6 +1378,71 @@ void runBenchMark()
         }
     }
 
+    // Encode MaxDays Constraints
+    // TODO: Make this work with variable amount of courses
+    int D = 4;
+    for (Distribution maxDaysDistribution : distributionsMap["MaxDays"])
+    {
+        vector<string> distributionClasses = maxDaysDistribution.classes;
+        for (size_t class1Index = 0; class1Index < distributionClasses.size(); class1Index++)
+        {
+            string class1Id = distributionClasses[class1Index];
+            Class class1 = classMap[class1Id];
+            int class1LiteralIndex = 0;
+            for (Class classObj : classVec)
+            {
+                if (classObj.id == class1.id)
+                    break;
+                class1LiteralIndex++;
+            }
+
+            for (size_t class2Index = class1Index + 1; class2Index < distributionClasses.size(); class2Index++)
+            {
+                string class2Id = distributionClasses[class2Index];
+                Class class2 = classMap[class2Id];
+                int class2LiteralIndex = 0;
+                for (Class classObj : classVec)
+                {
+                    if (classObj.id == class2.id)
+                        break;
+                    class2LiteralIndex++;
+                }
+                for (int class1TimingIndex = 0; class1TimingIndex < class1.timings.size(); class1TimingIndex++)
+                {
+                    Timing timing1 = class1.timings[class1TimingIndex];
+                    for (int class2TimingIndex = 0; class2TimingIndex < class2.timings.size(); class2TimingIndex++)
+                    {
+                        Timing timing2 = class2.timings[class2TimingIndex];
+                        bool constraintEncoded = false;
+                        int assignedDays = 0;
+                        for (int dayIndex = 0; dayIndex < days && !constraintEncoded; dayIndex++)
+                        {
+                            string timing1Days = timing1.days;
+                            string timing2Days = timing2.days;
+                            if (timing1Days[dayIndex] == '1' || timing2Days[dayIndex] == '1')
+                                assignedDays++;
+
+                            if (D < assignedDays)
+                            {
+                                int periodLit1 = t[class1LiteralIndex][class1TimingIndex];
+                                int periodLit2 = t[class2LiteralIndex][class2TimingIndex];
+                                if (verbose)
+                                    cout
+                                        << "[VERBOSE] Adding MaxDays(" << D << ") constraint: -" << periodLit1 << ", -" << periodLit2 << ", 0 \n";
+                                ipamirAddClause(solver,
+                                                {-periodLit1, -periodLit2},
+                                                literalCounter,
+                                                maxDaysDistribution.required,
+                                                maxDaysDistribution.penalty);
+                                constraintEncoded = true;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
     // Print answer and ask user for input
     while (true)
     {
