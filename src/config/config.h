b0d8e131379cae1e81a73053d26ac17733cc4ed4
Matti Kähkönen <matti.kahkonen99@gmail.com>
2026-03-12 16:10:16 +0200

    prune header file

--- src/config/config.h
+++ src/config/config.h
@@ -4,40 +4,9 @@
 #include <map>
 #include <string>
 #include "./DistributionTypes.h"
+#include "./ClassTypes.h"
 
 using namespace std;
-
-struct RoomUnavailability
-{
-    string days;
-    string weeks;
-    int start;
-    int length;
-};
-
-struct Room
-{
-    string id;
-    int penalty;
-    vector<RoomUnavailability> unavailability;
-    map<string, int> travelTimes;
-};
-
-struct Timing
-{
-    string days;
-    string weeks;
-    int start;
-    int length;
-    int penalty;
-};
-
-struct Class
-{
-    string id;
-    vector<Room> rooms;
-    vector<Timing> timings;
-};
 
 void generateConfig(vector<int> configVariables);
 vector<int> getConfigVariables();
